02bb18a0183c6c2c83cf7d9a593a7f7487fefe0b
Nixon.Shih <powerwolf543@gmail.com>
2020-08-22 17:38:10 +0800

    Add Xcode project

---
+++ RxSwiftPlayground/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//
+//  Created by Nixon Shih on 2020/8/21.
+//  Copyright © 2020 Nixon Shih. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
