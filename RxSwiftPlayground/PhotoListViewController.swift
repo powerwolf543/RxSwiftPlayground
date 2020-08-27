3e7ba2b87bfc98d41f2995d0cb9107c9a7feab92
Nixon.Shih <powerwolf543@gmail.com>
2020-08-27 21:09:35 +0800

    Add views for photo list

--- RxSwiftPlayground/PhotoListViewController.swift
+++ RxSwiftPlayground/PhotoListViewController.swift
@@ -5,8 +5,9 @@
 //  Copyright © 2020 Nixon Shih. All rights reserved.
 //
 
-import UIKit
+import Networking
 import RxSwift
+import UIKit
 
 final internal class PhotoListViewController: UIViewController {
     private let searchBar: UISearchBar = {
@@ -16,19 +17,52 @@ final internal class PhotoListViewController: UIViewController {
         return searchBar
     }()
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.register(PhotoTableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var dataSource: [PhotoInfo] = []
     private let disposeBag: DisposeBag = DisposeBag()
     
     // MARK: Function
+    
+    override func loadView() {
+        super.loadView()
+        view = tableView
+    }
     
     override internal func viewDidLoad() {
         super.viewDidLoad()
         
         setupUIs()
+        
+        HTTPClient().fetchDataModel(request: PhotosRequest()).observeOn(MainScheduler.instance).subscribe(onNext: { [unowned self] models in
+            self.dataSource = models
+            self.tableView.reloadData()
+        }).disposed(by: disposeBag)
     }
     
     private func setupUIs() {
         view.backgroundColor = UIColor(white: 29.0 / 255.0, alpha: 1.0)
         navigationItem.titleView = searchBar
+        
+        tableView.dataSource = self
     }
 }
 
+extension PhotoListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PhotoTableViewCell
+        let model = dataSource[indexPath.row]
+        cell.label.text = model.title
+        return cell
+    }
+}
---
+++ RxSwiftPlayground/PhotoTableViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  PhotoTableViewCell.swift
+//
+//  Created by Nixon Shih on 2020/8/26.
+//  Copyright © 2020 Nixon Shih. All rights reserved.
+//
+
+import UIKit
+
+internal final class PhotoTableViewCell: UITableViewCell {
+    internal let contentImageView: UIImageView = UIImageView()
+    
+    internal let label: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(white: 0.8, alpha: 1)
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let padding: CGFloat = 13
+    
+    override internal init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        separatorInset = .zero
+        backgroundColor = .clear
+        selectionStyle = .none
+        
+        contentImageView.translatesAutoresizingMaskIntoConstraints = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(contentImageView)
+        contentView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            contentImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            contentImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            label.topAnchor.constraint(equalTo: contentImageView.bottomAnchor, constant: padding),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            contentView.trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: padding),
+            contentView.bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: padding)
+        ])
+    }
+}
